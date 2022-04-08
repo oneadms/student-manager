2e76ef775b443e73b217389d105da47877b0e4d5
oneadm <60870148+oneadms@users.noreply.github.com>
2022-04-08 17:43:43 +0800

    班级状态码

---
+++ src/main/java/com/example/studentmanager/cmmon/ClazzStatus.java
@@ -0,0 +1,18 @@
+package com.example.studentmanager.cmmon;
+
+/**
+ * @author cnmgb
+ * @version 1.0
+ * @date 2022/4/8
+ **/
+public interface ClazzStatus {
+
+  int ADD_SUCCESS = 1;
+
+  int ADD_FAILURE = 2;
+  int DELETE_SUCCESS = 1;
+  int DELETE_FAILRFE = 2;
+  int CLAZZ_IN_EXISTS = 3;
+  int CLAZZ_NOT_EXITS = 4;
+
+}
