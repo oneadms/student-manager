fe0ae04faac8fa5c4d8aa83fcb184e737669b595
oneadm <60870148+oneadms@users.noreply.github.com>
2022-04-11 10:35:50 +0800

    登录成功后存入用户信息到session

--- src/main/java/com/example/studentmanager/service/UserService.java
+++ src/main/java/com/example/studentmanager/service/UserService.java
@@ -1,9 +1,9 @@
 package com.example.studentmanager.service;
 
 import com.example.studentmanager.cmmon.LoginStatus;
-import com.example.studentmanager.cmmon.Result;
 import com.example.studentmanager.model.dao.UserDao;
 import com.example.studentmanager.model.entity.User;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author cnmgb
@@ -13,12 +13,14 @@ import com.example.studentmanager.model.entity.User;
 
 public class UserService extends BaseService<UserDao> {
 
-    public int login(String username, String password) {
+    public int login(String username, String password, HttpServletRequest req) {
         User user = baseDao.getUserByUsername(username);
         if (user == null) {
             return LoginStatus.USER_NOT_FOUND;
         }else{
             if (user.getPassword().equals(password)) {
+                req.getSession().setAttribute("username", username);
+                req.getSession().setAttribute("nickname", user.getNickname());
                 return LoginStatus.LOGIN_SUCCESS;
             }
         }
