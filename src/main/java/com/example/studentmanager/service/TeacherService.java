377076c04238cbf034e11979986f3f13889d315d
oneadm <60870148+oneadms@users.noreply.github.com>
2022-04-11 10:38:42 +0800

    获取教师列表

---
+++ src/main/java/com/example/studentmanager/service/TeacherService.java
@@ -0,0 +1,27 @@
+package com.example.studentmanager.service;
+
+import com.example.studentmanager.model.dao.TeacherDao;
+import com.example.studentmanager.model.vo.RespPageBean;
+import java.sql.SQLException;
+import javafx.print.PageLayout;
+
+/**
+ * @author cnmgb
+ * @version 1.0
+ * @date 2022/4/11
+ **/
+public class TeacherService extends BaseService<TeacherDao>{
+
+  public RespPageBean getTeacherByPage(String page, String rows, String sort, String order) {
+    Long total = baseDao.getTotal();
+    int size = Integer.parseInt(rows);
+    int offset = Integer.parseInt(page) - 1;
+    int start= offset * size;
+    try {
+      return new RespPageBean(total, baseDao.getTeacherByPage(start, size, sort, order));
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return null;
+  }
+}
